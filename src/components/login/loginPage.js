d53ad4d52c605b91fef533e2cbb40ee00730d4a1
Shadab8989 <107117111+Shadab8989@users.noreply.github.com>
2024-01-10 14:49:09 +0530

    login functionality

--- src/components/login/loginPage.js
+++ src/components/login/loginPage.js
@@ -2,15 +2,21 @@ import React from 'react'
 import './login.css'
 import { useState } from 'react'
 
-function LoginPage() {
+function LoginPage({handleSubmit}) {
+  
   const [email,setEmail] = useState('')
   const [password,setPassword] = useState('')
+
+  const HandleSubmit = (event) => {
+    event.preventDefault()
+    handleSubmit(email,password)
+  }
   return (
     <div>
-    <form className='formClass'>
+    <form className='formClass' onSubmit={HandleSubmit}>
         <input type='email' value = {email} onChange={(e) => {setEmail(e.target.value)}} placeholder='Email' className='emailInput'></input>
         <input type='password' value={password} onChange={(e) => {setPassword(e.target.value)}} placeholder='Password' className='passwordInput'></input>
-        <button>Login</button>
+        <button type='submit'>Login</button>
     </form>
       
     </div>
